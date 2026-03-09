a8530604cacb02b36945b8050a61fc3b4db41d44
caioseguchi <caioseguchi@gmail.com>
2026-03-09 22:30:04 +0100

    Listar todos os usuarios

--- src/app/pages/user-get-all/user-get-all.ts
+++ src/app/pages/user-get-all/user-get-all.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header-component/header-component';
+import { UserService } from '../../service/user-service';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-user-get-all',
@@ -7,4 +9,28 @@ import { HeaderComponent } from '../../components/header-component/header-compon
   templateUrl: './user-get-all.html',
   styleUrl: './user-get-all.scss',
 })
-export class UserGetAll {}
+export class UserGetAll {
+  usuarios: any[] = [];
+
+  constructor(
+    private userService: UserService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit() {
+    this.getAllUsuarios();
+  }
+
+  getAllUsuarios() {
+    this.userService.getAllUsuarios().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.usuarios = data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+}
--- src/app/service/user-service.ts
+++ src/app/service/user-service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASE_URL = 'http://localhost:8080';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  constructor(private http: HttpClient) {}
+
+  getAllUsuarios(): Observable<any> {
+    return this.http.get<any[]>(`${BASE_URL}/usuario`);
+  }
 }
